646cf7040e54bf634729cdc4ca84bf6c25b695ce
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-12-21 16:57:05 -0500

    Move files

--- freecad/Animation/VertexTracker.py
+++ freecad/Animation/VertexTracker.py
@@ -2,22 +2,19 @@
 # SPDX-FileCopyrightText: 2015 microelly
 # SPDX-FileNotice: Part of the Animation addon.
 
+from importlib import reload
 import FreeCAD,PySide,os,FreeCADGui
-from PySide import QtCore, QtGui, QtSvg
+from PySide import QtCore, QtGui
 from PySide.QtGui import *
 import Part
 import Draft
 
 from FreeCAD import activeDocument
 
-
-#----------
-
-
-import math,os
+import os
 
 import FreeCAD, Animation, PySide
-from Animation import say,sayErr,sayexc
+from Animation import say
 from  EditWidget import EditNoDialWidget
 import Toucher
 reload(Toucher)
@@ -34,45 +31,55 @@ def createVertexTracker (
 
     document = activeDocument()
 
-    obj = document.addObject("App::DocumentObjectGroupPython",name)
-    obj.addProperty("App::PropertyString","filename","Base","").filename=filename
-    obj.addProperty("App::PropertyLink","src","Base","").src=src
-    obj.addProperty("App::PropertyFloat","time","Base","").time=0
+    if not document :
+        return
+
+    obj = document.addObject('App::DocumentObjectGroupPython',name)
+    obj.addProperty('App::PropertyString','filename','Base','').filename=filename
+    obj.addProperty('App::PropertyLink','src','Base','').src=src
+    obj.addProperty('App::PropertyFloat','time','Base','').time=0
 
     _VertexTracker(obj)
     _ViewProviderVertexTracker(obj.ViewObject)
     return obj
 
 
-class _VertexTracker(Animation._Actor):
-    ''' track the time/placement of src to filename '''
+class _VertexTracker ( Animation._Actor ):
 
-    def update(self):
-        print("update VERTEX Tracker ")
+    '''
+    Track the time / placement of src to filename
+    '''
+
+    def update ( self ):
+        print('Update VERTEX Tracker')
         self.run(self.obj2.src)
 
-    def step(self,now):
-        say("step "+str(now) + str(self))
-        self.obj2.time=float(now)/100
+    def step ( self , now ):
+        say(f'step { now }{ self }')
+        self.obj2.time = float(now) / 100
 
+    def addpoint ( self , point ):
 
-    #-----------------------------------------
-
-    def addpoint(self,point):
         try:
             self.threads
         except:
-            self.threads={}
-        fcount=len(self.threads)
-        dist=1.5
+            self.threads = {}
+
+        fcount = len(self.threads)
+        dist = 1.5
+
         for f in self.threads:
+
             print(f, self.threads[f])
+
             t=self.threads[f]
+
             if point.distanceToPoint(t[-1]) < dist:
-                print( "found")
+                print( 'found')
                 self.threads[f].append(point)
                 return self.threads[f]
-        print("not found")
+
+        print('not found')
         self.threads[fcount]=[point]
         return self.threads[fcount]
 
@@ -85,7 +92,7 @@ class _VertexTracker(Animation._Actor):
         for v in s.Shape.Vertexes:
             i += 1
             if i > 5: break
-            say(["Point: ", v.Point])
+            say(['Point: ', v.Point])
             FreeCADGui.updateGui()
             self.addpoint(v.Point)
 
@@ -93,13 +100,13 @@ class _VertexTracker(Animation._Actor):
         for f in self.threads:
             print(f)
             for p in self.threads[f]:
-                print("       ",p)
+                print('       ',p)
 
     def gen(self):
         for f in self.threads:
             print(f)
             if  self.isOnePoint(self.threads[f]):
-                p=FreeCAD.ActiveDocument.addObject("Part::Vertex","Vertex")
+                p=FreeCAD.ActiveDocument.addObject('Part::Vertex','Vertex')
                 p.Placement.Base=self.threads[f][0]
             else:
                 Draft.makeWire(self.threads[f])
@@ -130,9 +137,9 @@ class _ViewProviderVertexTracker(Animation._ViewProviderActor):
         return __dir__ +'/icons/icon2.svg'
 
     def attach(self,vobj):
-        self.emenu=[["Show Path Data",self.Object.Proxy.show],["Generate Path",self.Object.Proxy.gen]]
+        self.emenu=[['Show Path Data',self.Object.Proxy.show],['Generate Path',self.Object.Proxy.gen]]
         self.cmenu=self.emenu
-        say("attach " + str(vobj.Object.Label))
+        say('attach ' + str(vobj.Object.Label))
         self.Object = vobj.Object
         self.obj2=self.Object
         self.Object.Proxy.Lock=False
@@ -145,7 +152,7 @@ class _ViewProviderVertexTracker(Animation._ViewProviderActor):
 
     def showVersion(self):
         cl=self.Object.Proxy.__class__.__name__
-        PySide.QtGui.QMessageBox.information(None, "About ", "Animation" + cl +" Node\nVersion " + __vers__ )
+        PySide.QtGui.QMessageBox.information(None, 'About ', 'Animation' + cl +' Node\nVersion ' + __vers__ )
 
     def showpath(self):
         ''' path as Part.polygon '''
